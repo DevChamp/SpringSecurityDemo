40209af44709fe93993f06e009ab1f2c4935b796
Chetan Pardeshi <chetan@enact-systems.com>
2018-04-09 14:35:48 +0530

    warning added

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -14,37 +14,38 @@ import com.example.paypal.PayPalSuccess;
 @Controller
 public class TestController {
 
-	@RequestMapping(value="/test/{param}", method=RequestMethod.GET)
+	@RequestMapping(value = "/test/{param}", method = RequestMethod.GET)
 	@ResponseBody
 	public String test(@PathParam("param") String param) {
 		return encryptPassword("TSOEZa17");
-//		return "success !";
+		// return "success !";
 	}
-	
+
 	public static String encryptPassword(String password) {
-        org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder passwordEncoder = new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
-        return passwordEncoder.encode(password);
-    }
-	
+		org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder passwordEncoder = new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
+		return passwordEncoder.encode(password);
+	}
+
 	@RequestMapping(value = "/dashboard", method = RequestMethod.GET)
-	public String success(ModelMap modelMap, HttpServletRequest request){
-		 System.out.println(request.getParameter("payment_gross"));
-	     PayPalSuccess  payPalSucess = new PayPalSuccess();
-		 modelMap.put("result", payPalSucess.getPayPal(request));
-	   	 return "success";
-}
-	
-	@RequestMapping(value= {"/home","/"}, method=RequestMethod.GET)
+	public String success(ModelMap modelMap, HttpServletRequest request) {
+		System.out.println(request.getParameter("payment_gross"));
+		PayPalSuccess payPalSucess = new PayPalSuccess();
+		modelMap.put("result", payPalSucess.getPayPal(request));
+		return "success";
+	}
+
+	@RequestMapping(value = { "/home", "/" }, method = RequestMethod.GET)
 	public String home() {
+		int i;
 		return "home";
 	}
-	
-	@RequestMapping(value="/hello", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String hello() {
 		return "hello";
 	}
-	
-	@RequestMapping(value="/login", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
 		return "login";
 	}
